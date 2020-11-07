1cf0ea02b7f7a8cc65bb014bf902e19d007c51b6
boynuibon3s <boynuibon3s@gmail.com>
2020-11-07 15:49:18 +0700

    video 1 c1  update

--- B1_Training/v1-1/app.js
+++ B1_Training/v1-1/app.js
@@ -7,9 +7,11 @@ const port = 5500;              //nếu thay đổi thứ gì đó trong đây (
 const server = http.createServer((req, res) => {  //require // respond (kết quả trả về)
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello lmao');           //trả về g trị hello world
+  res.end('Hello lmao');           //trả về g trị hello world trên trang html
 });
 
 server.listen(port, hostname, () => {       //server lắng nghe port là 3000, hostname
-  console.log(`Server running at http://${hostname}:${port}/`);   //in ra giá trị server đang lắng nghe
+  console.log(`Server running at http://${hostname}:${port}/`);   //in ra giá trị server đang lắng nghe ở bảng terminal
 });
+
+/**server sẽ run trên local host tại hostname và port */
