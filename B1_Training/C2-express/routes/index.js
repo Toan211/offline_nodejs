ab2d999647e7867d07076afe1c6cd0ac9ccd6cc6
boynuibon3s <boynuibon3s@gmail.com>
2020-11-10 10:37:12 +0700

    change

--- B1_Training/C2-express/routes/index.js
+++ B1_Training/C2-express/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require('express');   //*express is a framework, provide us many useful thing
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });  //*index is in view folder
 });
 
 module.exports = router;
