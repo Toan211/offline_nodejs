778cac6c73091a3e00feaaba5ee9da210ca86489
boynuibon3s <boynuibon3s@gmail.com>
2020-11-10 11:31:11 +0700

    Router

--- B1_Training/C2-express/app.js
+++ B1_Training/C2-express/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//!1C2-2-Router
+var videosRouter = require('./routes/video')
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter);     
+//!1C2-2-Router
+app.use('/video', videosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- B1_Training/C2-express/routes/index.js
+++ B1_Training/C2-express/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });  //*index is in view folder
+  console.log("ABCABC123");
+  res.render('index', { title: 'Express 123123' });  //*index is in view folder
 });
 
 module.exports = router;
