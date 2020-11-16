31725f4a781e22441740be9822be999d28e5d686
boynuibon3s <boynuibon3s@gmail.com>
2020-11-16 13:43:27 +0700

    1C2-3-Template.4

--- B1_Training/C2-express/app.js
+++ B1_Training/C2-express/app.js
@@ -1,9 +1,15 @@
+//package from system
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+//package user install from outside
+var expressLayouts = require('express-ejs-layouts');
+
+//package user make
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,6 +18,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
